62a0c5c50943a19302a91342bba1bf250207dbb4
root <root@cnas>
2022-10-16 18:19:41 +0800

    update

--- app/template/base/commonJs.tpl.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="<?=$webRoot?>/static/js/jquery.min.js"></script>
-<script type="text/javascript" src="<?=$webRoot?>/static/js/bootstrap.js"></script>
-<script type="text/javascript" src="<?=$webRoot?>/static/js/main.js"></script>
--- app/template/base/header.tpl.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript">
-    var webRoot = '<?=$webRoot?>';
-</script>
-<body>
